71d85001761f0ead3790fd2e1fa5c8f8690fdec4
tsingwong <919916459@qq.com>
2017-11-13 23:08:51 +0800

    inity, one Entry One Output

---
+++ src/entry.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: tsingwong 
+ * @Date: 2017-11-13 22:18:39 
+ * @Last Modified by: tsingwong
+ * @Last Modified time: 2017-11-13 22:20:39
+ */
+document.getElementById('title').innerHTML = 'TsingWong';
