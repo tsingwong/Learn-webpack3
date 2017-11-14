c9b88692c522f9f174c21e73c2726e0b6c5d721d
tsingwong <919916459@qq.com>
2017-11-14 18:35:53 +0800

    webpack.config.js init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: tsingwong 
+ * @Date: 2017-11-14 18:30:58 
+ * @Last Modified by: tsingwong
+ * @Last Modified time: 2017-11-14 18:34:25
+ */
+module.exports = {
+    entry: {
+
+    },
+    output: {
+        
+    },
+    module: {
+        
+    },
+    plugins: [
+
+    ],
+    devServer: {
+        
+    }
+};
