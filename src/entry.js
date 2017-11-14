9d3c02e9eae506baf6e475c85e26e08b53408bcb
tsingwong <919916459@qq.com>
2017-11-14 22:33:40 +0800

    add devServer config

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,6 @@
  * @Author: tsingwong 
  * @Date: 2017-11-13 22:18:39 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-11-13 22:20:39
+ * @Last Modified time: 2017-11-14 22:29:26
  */
-document.getElementById('title').innerHTML = 'TsingWong';
+document.getElementById('title').innerHTML = 'TsingWong~~';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: tsingwong 
  * @Date: 2017-11-14 18:30:58 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-11-14 20:46:51
+ * @Last Modified time: 2017-11-14 22:30:34
  */
 const path = require('path');
 module.exports = {
@@ -21,6 +21,13 @@ module.exports = {
 
     ],
     devServer: {
-
+        // 绝对路径可以保证各个系统一致
+        contentBase: path.resolve(__dirname, 'dist'),
+        // 获取当前的ip
+        host: process.env.HOST,
+        // 开启 gzip 压缩
+        compress: true,
+        // 默认端口是 8080
+        port: 8081
     }
 };
