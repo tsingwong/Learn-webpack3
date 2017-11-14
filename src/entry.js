367ecb4020266d91c26fe32b2cd346aca71141a8
tsingwong <919916459@qq.com>
2017-11-14 22:58:08 +0800

    pack the css code

--- src/entry.js
+++ src/entry.js
@@ -2,6 +2,8 @@
  * @Author: tsingwong 
  * @Date: 2017-11-13 22:18:39 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-11-14 22:29:26
+ * @Last Modified time: 2017-11-14 22:41:19
  */
+import css from './css/index.css';
+
 document.getElementById('title').innerHTML = 'TsingWong~~';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: tsingwong 
  * @Date: 2017-11-14 18:30:58 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-11-14 22:30:34
+ * @Last Modified time: 2017-11-14 22:55:11
  */
 const path = require('path');
 module.exports = {
@@ -15,7 +15,26 @@ module.exports = {
         filename: '[name].js',
     },
     module: {
-        
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ],
+                // 必处理文件
+                include: /src/,
+                // 必不处理文件
+                exclude: /node_modules/,
+                // query 额外配置
+                // query:
+            }
+        ]
     },
     plugins: [
 
@@ -28,6 +47,10 @@ module.exports = {
         // 开启 gzip 压缩
         compress: true,
         // 默认端口是 8080
-        port: 8081
+        port: 8081,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
     }
 };
