07caf4ba3b406ef93188aa46bc31b4d8dca6472c
tsingwong <919916459@qq.com>
2017-11-14 20:52:53 +0800

    add two entry & two output

---
+++ src/entry2.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: tsingwong 
+ * @Date: 2017-11-14 19:03:33 
+ * @Last Modified by: tsingwong
+ * @Last Modified time: 2017-11-14 19:03:55
+ */
+/* eslint-disable */
+console.log('this is entry2.js!');
+/* eslint-enable */
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@
  * @Author: tsingwong 
  * @Date: 2017-11-14 18:30:58 
  * @Last Modified by: tsingwong
- * @Last Modified time: 2017-11-14 18:34:25
+ * @Last Modified time: 2017-11-14 20:46:51
  */
+const path = require('path');
 module.exports = {
     entry: {
-
+        one: './src/entry.js',
+        two: './src/entry2.js',
     },
     output: {
-        
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
     },
     module: {
         
@@ -18,6 +21,6 @@ module.exports = {
 
     ],
     devServer: {
-        
+
     }
 };
